13304ece20b07c0017f5913315c14c493d302408
Chelsea L <chelsealumsden@gmail.com>
2021-07-06 13:34:20 -0400

    WIP: Main Navigation with links

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,27 @@
+import {Link} from 'react-router-dom';
 
 
 function Main(props){
-    return <h1>Main Place </h1>;
+    return (
+        <div>
+            <Link to='/foods'>
+                <h2>Food</h2>
+            </Link>
+            <Link to='/bars'>
+                <h2>Bars/ Lounges</h2>
+            </Link>
+            <Link to='/clubs'>
+                <h2>Gentlemen Clubs</h2>
+            </Link>
+            <Link to='/parks'>
+                <h2>Parks</h2>
+            </Link>
+            <Link to='/attractions'>
+                <h2>Attractions</h2>
+            </Link>
+            
+        </div>
+    );
   } 
   
   export default Main;
