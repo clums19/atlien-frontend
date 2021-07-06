8612f9e076993c689541d8d4fe85643f5e07c3d5
Chelsea L <chelsealumsden@gmail.com>
2021-07-06 12:50:44 -0400

    WIP: Paths of Home and Main pages updated and linked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import Main from './components/Main';
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/nav'>
+          <Main />
+        </Route>
+      </Switch>
+      
     </div>
+    
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,9 @@
 import {Link} from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import Main from './Main';
 
 function Home(props) {
     return (
         <div className='indexNav'>
             <h1>ATLien: Welcome to Atlanta</h1>
-            <Route path='/nav'>
-                <Main />
-            </Route>
             <Link to='/nav'>
                 <div>Explore</div>
             </Link>
