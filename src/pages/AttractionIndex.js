3b08748a875622280ab33de0b38c3568a1d27472
Chelsea L <chelsealumsden@gmail.com>
2021-07-08 08:54:57 -0400

    WIP: Popup functional for Attractions

--- src/pages/AttractionIndex.js
+++ src/pages/AttractionIndex.js
@@ -1,11 +1,30 @@
+import Popup from "reactjs-popup";
+import {Link} from 'react-router-dom';
+
 
 function AttractionIndex(props){
 
   const loaded = () => {
+    
     return props.attraction.map((attraction) => (
-        <div key={attraction._id} className='foods'>
+      <div key={attraction._id} className='foods'>
           <div id='section'>
-            <span>{attraction.title}</span>
+      <Popup
+          trigger={<span className='span'>{attraction.title}</span>}
+          modal
+          nested>
+        <div className="header">{attraction.title}</div>
+            <div className="content">
+              {' '}
+              {attraction.description}
+              <br />
+              <p>Distance: {attraction.distance}</p>
+              <br />
+              <Link to={attraction.web}>
+                Click to go to Website
+              </Link> 
+            </div>
+      </Popup>
             {attraction.image && <img src={attraction.image} alt={attraction.title}/>}
           </div>
         </div>
