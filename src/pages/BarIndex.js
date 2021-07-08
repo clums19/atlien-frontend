7cbe573b36e1fcf0cac55362cf76b02a044f9ecd
Chelsea L <chelsealumsden@gmail.com>
2021-07-08 09:36:54 -0400

    WIP: Bar Popup functional

--- src/pages/BarIndex.js
+++ src/pages/BarIndex.js
@@ -1,10 +1,40 @@
+import Popup from "reactjs-popup";
+
 function BarIndex(props){
 
   const loaded = () => {
       return props.bar.map((bar) => (
           <div key={bar._id} className='foods'>
             <div id='section'>
-              <span className="title">{bar.title}</span>
+            <Popup
+              trigger={<span className='span'>{bar.title}</span>}
+              modal
+              nested>
+              {close => (
+                <div className="modal">
+                  <button className="close" onClick={close}>
+                    &times;
+                  </button>
+                <div className="header">{bar.title}</div>
+                    <div className="content">
+                      {' '}
+                        {bar.description}
+                      <br />
+                        <p>Distance: {bar.distance} from Atlanta Center</p>
+                      <br />
+                        <a href={bar.web} target='_blank' rel='noreferrer'>
+                          Click to go to Website
+                        </a> 
+                    </div>
+                <button
+                  className="button"
+                  onClick={() => {
+                    close();
+                  }}> Close
+                </button>
+                </div>
+                )}
+              </Popup>
               {bar.image && <img src={bar.image} alt={bar.title}/>}
             </div>
           </div>
